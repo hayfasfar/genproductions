cefd0771e6017db8fd07e121ddef832f5b6d19cb
UAEDF-tomc <tom.cornelis@ua.ac.be>
2017-10-19 17:26:42 +0200

    Adding more options to makeHeavyNeutrino card script: onshell, pre2017, samesign

--- bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/exo_heavyNeutrino/makeHeavyNeutrinoCards.py
+++ bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/exo_heavyNeutrino/makeHeavyNeutrinoCards.py
@@ -6,16 +6,19 @@ def replaceInCard(card, replacements):
   for r in replacements:      data = data.replace(r[0], r[1])
   with open(card, 'w') as f:  f.write(data)
 
-def makeHeavyNeutrinoCards(mass, coupling, flavours):
-  baseName = 'HeavyNeutrino_trilepton_M-' + str(mass) + '_V-' + str(coupling) + '_' + flavours + '_NLO'
+def makeHeavyNeutrinoCards(mass, coupling, flavours, onshell, isPre2017, type):
+  baseName = 'HeavyNeutrino_' + type + '_M-' + str(mass) + '_V-' + str(coupling) + '_' + flavours + ('_onshell' if onshell else '') + ('_pre2017' if isPre2017 else '') + '_NLO'
 
   try:    os.makedirs(baseName)
   except: pass
 
   for card in ['madspin_card', 'extramodels', 'run_card', 'proc_card', 'customizecards']:
-    shutil.copyfile('templateCards/HeavyNeutrino_trilepton_template_NLO_' + card + '.dat', baseName + '/' + baseName + '_' + card + '.dat')
+    shutil.copyfile('templateCards/HeavyNeutrino_template_NLO_' + card + '.dat', baseName + '/' + baseName + '_' + card + '.dat')
 
-  replacements = [('MASS', str(mass)), ('COUPLING', str(coupling)), ('FLAVOURS', flavours)]
+  replacements = [('MASS',     str(mass)),
+                  ('COUPLING', str(coupling)),
+                  ('FLAVOURS', flavours),
+                  ('SPINMODE', 'onshell' if onshell else 'none')]
 
   if flavours == '2l':    replacements += [('l+ = e+ mu+ ta+', 'l+ = e+ mu+'), ('l- = e- mu- ta-', 'l- = e- mu-')]
   elif flavours == 'e':   replacements += [('l+ = e+ mu+ ta+', 'l+ = e+'),     ('l- = e- mu- ta-', 'l- = e-')]
@@ -23,19 +26,32 @@ def makeHeavyNeutrinoCards(mass, coupling, flavours):
   elif flavours == 'tau': replacements += [('l+ = e+ mu+ ta+', 'l+ = ta+'),    ('l- = e- mu- ta-', 'l- = tau-')]
 
   if flavours in ['e','mu','2l']:    replacements += [('ldecay+ = e+ mu+ ta+', 'ldecay+ = e+ mu+'), ('ldecay- = e- mu- ta-', 'ldecay- = e- mu-')]
-
   if flavours in ['3l', '2l', 'e']:  replacements += [('set param_card numixing 1 0.000000e+00', 'set param_card numixing 1 %E' % coupling)]
   if flavours in ['3l', '2l', 'mu']: replacements += [('set param_card numixing 4 0.000000e+00', 'set param_card numixing 4 %E' % coupling)]
   if flavours in ['3l', 'tau']:      replacements += [('set param_card numixing 7 0.000000e+00', 'set param_card numixing 7 %E' % coupling)]
 
+  if isPre2017:
+    replacements += [('$DEFAULT_PDF_SETS', '292200')]
+    replacements += [('$DEFAULT_PDF_MEMBERS', '292201  =  PDF_set_min\n292302  =  PDF_set_max\nTrue')]
+
+  if type=='samesign':
+    replacements += [('decay n1 > ldecay ldecay v', 'decay n1 > ldecay j j')]
+
+
+  replaceInCard(baseName + '/' + baseName + '_run_card.dat',       replacements)
   replaceInCard(baseName + '/' + baseName + '_proc_card.dat',      replacements)
   replaceInCard(baseName + '/' + baseName + '_customizecards.dat', replacements)
+  replaceInCard(baseName + '/' + baseName + '_madspin_card.dat',   replacements)
 
 
-combinations = [
-  (1,     0.11557,     'e'),
-  (1,     0.11505,     'mu')
-]
+# Example:
+if __name__ == "__main__":
+  combinations = [
+    (1,     0.11557,     'e',   True,    False, 'trilepton'),
+    (1,     0.11505,     'mu',  True,    False, 'trilepton'),
+    (1,     0.11505,     'mu',  True,    True,  'samesign'),
+    (1,     0.11505,     'mu',  True,    True,  'trilepton')
+  ]
 
-for mass, coupling, flavour in combinations:
-  makeHeavyNeutrinoCards(mass, coupling, flavour)
+  for mass, coupling, flavour, spinmode, isPre2017, type in combinations:
+    makeHeavyNeutrinoCards(mass, coupling, flavour, spinmode, isPre2017, type)
