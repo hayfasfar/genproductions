bddcd9d1dc6f2dc8a6278a3f4a93b9d285c820dc
UAEDF-tomc <tom.cornelis@ua.ac.be>
2017-10-20 14:04:23 +0200

    Fix output directory, add some comments to the card generation script

--- bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/exo_heavyNeutrino/makeHeavyNeutrinoCards.py
+++ bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/exo_heavyNeutrino/makeHeavyNeutrinoCards.py
@@ -6,6 +6,16 @@ def replaceInCard(card, replacements):
   for r in replacements:      data = data.replace(r[0], r[1])
   with open(card, 'w') as f:  f.write(data)
 
+#
+# Create heavyNeutrino cards for given parameters
+# Returns baseName, which can be used to find the cards i.e. as baseName/baseName_*.dat
+# mass       - mass of the heavy neutrino particle
+# coupling   - mixing parameter between the heavy neutrino and lepton
+# flavours   - could be e, mu, tau, 2l (e+mu), 3l (e+mu+tau)
+# onshell    - if true, MadSpin onshell options is used, otherwise 'none'
+# isPre2017  - use older pdf's as used in Moriond17 campaign
+# type       - trilepton (n1 --> llnu) or lljj (n1 --> ljj)
+#
 def makeHeavyNeutrinoCards(mass, coupling, flavours, onshell, isPre2017, type):
   baseName = 'HeavyNeutrino_' + type + '_M-' + str(mass) + '_V-' + str(coupling) + '_' + flavours + ('_onshell' if onshell else '') + ('_pre2017' if isPre2017 else '') + '_NLO'
 
@@ -18,7 +28,9 @@ def makeHeavyNeutrinoCards(mass, coupling, flavours, onshell, isPre2017, type):
   replacements = [('MASS',     str(mass)),
                   ('COUPLING', str(coupling)),
                   ('FLAVOURS', flavours),
-                  ('SPINMODE', 'onshell' if onshell else 'none')]
+                  ('SPINMODE', 'onshell' if onshell else 'none'),
+                  ('TYPE',     type),
+                  ('EXTRA',    ('_onshell' if onshell else '') + ('_pre2017' if isPre2017 else ''))]
 
   if flavours == '2l':    replacements += [('l+ = e+ mu+ ta+', 'l+ = e+ mu+'), ('l- = e- mu- ta-', 'l- = e- mu-')]
   elif flavours == 'e':   replacements += [('l+ = e+ mu+ ta+', 'l+ = e+'),     ('l- = e- mu- ta-', 'l- = e-')]
@@ -34,7 +46,7 @@ def makeHeavyNeutrinoCards(mass, coupling, flavours, onshell, isPre2017, type):
     replacements += [('$DEFAULT_PDF_SETS', '292200')]
     replacements += [('$DEFAULT_PDF_MEMBERS', '292201  =  PDF_set_min\n292302  =  PDF_set_max\nTrue')]
 
-  if type=='samesign':
+  if type=='lljj':
     replacements += [('decay n1 > ldecay ldecay v', 'decay n1 > ldecay j j')]
 
 
@@ -43,15 +55,17 @@ def makeHeavyNeutrinoCards(mass, coupling, flavours, onshell, isPre2017, type):
   replaceInCard(baseName + '/' + baseName + '_customizecards.dat', replacements)
   replaceInCard(baseName + '/' + baseName + '_madspin_card.dat',   replacements)
 
+  return baseName
 
-# Example:
+#
+# Use Example:
+#
 if __name__ == "__main__":
-  combinations = [
+  argsList = [
     (1,     0.11557,     'e',   True,    False, 'trilepton'),
     (1,     0.11505,     'mu',  True,    False, 'trilepton'),
-    (1,     0.11505,     'mu',  True,    True,  'samesign'),
+    (1,     0.11505,     'mu',  True,    True,  'lljj'),
     (1,     0.11505,     'mu',  True,    True,  'trilepton')
   ]
 
-  for mass, coupling, flavour, spinmode, isPre2017, type in combinations:
-    makeHeavyNeutrinoCards(mass, coupling, flavour, spinmode, isPre2017, type)
+  for args in argsList: makeHeavyNeutrinoCards(*args)
